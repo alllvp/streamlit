586163edddd4bcd4e1c64641fdd5f02c792eda2b
alllvp <101820217+alllvp@users.noreply.github.com>
2024-05-16 21:11:38 +0200

    Update streamlit-test.py

--- streamlit-test.py
+++ streamlit-test.py
@@ -1,32 +1,32 @@
 import streamlit as st
 import pickle
-import sklearn
+import pandas as pd
+
+# Загрузка модели
+model = pickle.load(open('trained_pipe_knn.sav', 'rb'))
 
 st.title("Housing Prices Prediction")
 
 st.write("""
 ### Project description
 We have trained several models to predict the price of a house based on features such as the area of the house and the condition and quality of their different rooms.
-
 """)
 
+# Ввод данных пользователем
+LotArea = st.number_input("Lot Area", min_value=0)
+TotalBsmtSF = st.number_input("Basement Square Feet", min_value=0)
+BedroomAbvGr = st.number_input("Number of Bedrooms", min_value=0)
+GarageCars = st.number_input("Car spaces in Garage", min_value=0)
 
-model = pickle.load(open('trained_pipe_knn.sav', 'rb'))
-
-LotArea = st.number_input("Lot Area")
-TotalBsmtSF = st.number_input("Basement Square Feet")
-BedroomAbvGr = st.number_input("Number of Bedrooms")
-GarageCars = st.number_input("Car spaces in Garage")
-
-import pandas as pd
+# Формирование данных для предсказания
 new_house = pd.DataFrame({
-    'LotArea':[LotArea],
-    'TotalBsmtSF':[TotalBsmtSF],
-    'BedroomAbvGr':[BedroomAbvGr],
-    'GarageCars':[GarageCars]
+    'LotArea': [LotArea],
+    'TotalBsmtSF': [TotalBsmtSF],
+    'BedroomAbvGr': [BedroomAbvGr],
+    'GarageCars': [GarageCars]
 })
 
-prediction = model.predict(new_house)
-
-st.write("The price of the house is:", prediction)
-
+# Предсказание
+if st.button("Predict"):
+    prediction = model.predict(new_house)
+    st.write("The predicted price of the house is:", prediction[0])
